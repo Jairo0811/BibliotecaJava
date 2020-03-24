c21babc1e0a6dd21760c4cb38e81417ea630bfc8
Francis Jairo Matías Rosario <jairo_1829@hotmail.com>
2020-03-24 18:39:15 -0400

    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+Use SistemaBiblioteca;
+select  * from Cliente;
+
+insert into Cliente values ('1235','Jairo', 'Matias');
+insert into Cliente values ('1564','Orlando', 'Dominici');
+insert into Cliente values ('8965','Eduandy', 'Cruz');
+insert into Cliente values ('9874','Reidy', 'Charles');
